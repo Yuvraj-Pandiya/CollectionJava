bc040ffb702714f25c0f8d8102dea6337cbdb30c
yuvrajpandiya <pandiyayuvraj@gmail.com>
2026-02-04 17:02:07 +0530

    constructor and basic method of HashSet

--- HashSetDemo.java
+++ HashSetDemo.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.ArrayList;
 class HashSetDemo
 {
 	public static void main(String[]args)
@@ -28,5 +29,107 @@ class HashSetDemo
 		// 2. HashSet h2=new HashSet(int initialCapacity);
 		/* Creates an empty hashset object with specified initial capacity
 		 and default fill ratio 0.75*/
+		 HashSet h2=new HashSet(24);
+		 System.out.println(h2);
+		 
+		 // 3. HashSet h2=new HashSet(int initialCapacity,float FillRatio);
+		/* Creates an empty hashset object with specified initial capacity
+		 and specified fill ratio 
+		 If we pass wrong means in place of load factor negative value then
+		 we will get IllegalArgumentException:Illegal Load Factor : -1.0
+		 For Load Factor:-
+		 Lower values → less chance of collisions but more memory usage.
+		 Higher values → more compact but higher chance of collisions.
+		 */
+		 HashSet h3=new HashSet(4,2.0f);
+		 /*
+		 Initial capacity = 4 buckets.
+		 Threshold = 4 × 2.0 = 8.
+		 You can insert up to 8 elements before resizing happens.
+		 But since there are only 4 buckets, collisions will pile
+		 up in those buckets → performance suffers.
+		 
+		 Resizing HAppend when size > capacity * loadfactor
+         by factor of new capacity => old capacity * 2 ;	 
+		 */
+		 System.out.println(h3);
+		 
+		 /*
+		 HashSet h4= new HashSet(Collection c1);
+		 Creates an equivalent HashSet for the given Collection.
+		 This constructor meant for interconversion b/w Collection
+		 Objects;
+		 */
+		 HashSet h4= new HashSet(h2);
+		 System.out.println(h4);
+		 
+		 /*
+		 Note: FillRatio/LoadFactor-- After filling how much ratio a new HashSet 
+		 Object will be created, this ratio is called l.f or f.r;
+		 */
+		 
+		 h1.add("C");
+		 h1.add("B");
+		 h1.add("Z");
+		 h1.add(null);
+		 h1.add("D");
+		 h1.add(10);
+		 System.out.println(h1.add("Z")); // Return false becoz 
+		 // add from Collection and return boolean duplicate cant
+		 //be inserted in HashSet i.e why false returning
+		 System.out.println(h1);
+		 
+		 
+		 // Duplicates got removed from arraylist while converting to HashSet
+		 ArrayList l1=new ArrayList();
+		 l1.add(12);
+		 l1.add(2);
+		 l1.add(42);
+		 l1.add("Ram");
+		 l1.add("Ram");
+		 l1.add(2);
+		 l1.add(6);
+		 l1.add(98);
+		 l1.add(null);
+		 l1.add(null);
+		 HashSet h5=new HashSet(l1);
+		 System.out.println(l1);
+		 System.out.println(h5);
+		 
+		 /*5. Constructor 
+		 java.util.HashSet(int ,float,boolean);
+		 We can not use this constructor hashset internally uses this 
+		 constructor.
+		 */
+		 
+		 //boolean contains(Object) method 
+		 System.out.println(h5.contains(2));
+		 System.out.println(h5.contains(200));
+		 
+		 //boolean remove(Object) Collection remove method
+		 System.out.println(h5.remove(2));
+		 System.out.println(h5.remove(200));
+		 
+		 //int size()
+		 System.out.println(h5.size());
+		 
+		 // System.out.println(h5.capacity()); //Error
+		 
+		 //void clear()
+		 System.out.println(h5);
+		 h5.clear();
+		 System.out.println(h5);
+		 
+		 //boolean isEmpty()
+		 System.out.println(h5.isEmpty());
+		 h5.add(34);
+		 h5.add(3);
+		 h5.add(7);
+		 h5.add("dadda");
+		 System.out.println(h1.isEmpty());
+		 System.out.println(h5.isEmpty());
+		 
+		 //Next Topic is RabinKarpAlgorithm..HashSet 
+		 // (hashCode,equals,toString)
 	}
 } 
--- Practise.java
+++ Practise.java
@@ -3,15 +3,56 @@ class Practise
 {
 	public static void main(String[]args)
 	{
-		ArrayList al=new ArrayList();
-		al.add(10);
-		al.add(20);
-		al.add(30);
-		al.add(40);
-		al.add(50);
-		System.out.println(al);
-		System.out.println(al.remove(2));
-		System.out.println(al.remove("40"));
-		System.out.println(al);
+		HashSet h=new HashSet();
+		h.add(new Employee(10,"aaa"));
+		h.add(new Employee(10,"aaa"));
+		h.add(new Employee(10,"aaa"));
+		//Teno Object ka Refernce Alga he isiliye add ho jayege teno.
+		System.out.println(h);
+		//Befor overriding toString() output:
+		// [Employee@30f39991, Employee@452b3a41, Employee@a09ee92]
+
+		
+		//Teno alg alg object he isisliye teen bar 10 arha he 
+		System.out.println(h); //[10,10,10]
+		
+		// Step2
+		System.out.println(h); //[10,10,10]
+		
+		//Step3.
+		
+		System.out.println(h); //[10,10,10]
+		// if equals return false then this output
+		
+		System.out.println(h); //[10]
+		// if equals return true then this output
+	}
+}
+class Employee
+{
+	int id;
+	String name;
+	Employee(int id,String name)
+	{
+		this.id=id;
+		this.name=name;
+	}
+	// Step 1 
+	public String toString()
+	{
+			return ""+id;
+	}
+	
+	// Step 2
+	public int hashCode()
+	{
+		return 50;
+	}
+	
+	//Step 3
+	public boolean equals(Object o)
+	{
+		// return false;
+		return true;
 	}
 }
