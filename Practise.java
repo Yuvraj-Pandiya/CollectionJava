36db5abf9daf536cac8bc103568f7eb4d613c8ce
yuvrajpandiya <pandiyayuvraj@gmail.com>
2026-02-06 16:43:11 +0530

    HashSet Working in Practise.java

--- Practise.java
+++ Practise.java
@@ -1,4 +1,4 @@
-import java.util.*;
+/* import java.util.*;
 class Practise
 {
 	public static void main(String[]args)
@@ -54,5 +54,87 @@ class Employee
 	{
 		// return false;
 		return true;
+	}
+} */
+
+
+import java.util.*;
+class Practise
+{
+	public static void main(String[]args)
+	{
+		HashSet h= new HashSet();
+		// // Case1
+		// h.add(101);
+		// System.out.println(h);
+		// h.add(new Employee(101,"aaa"));
+		// // Exception : CCE (ClassCastException)
+		// System.out.println(h);
+		
+			// Case2
+		// h.add(new Employee(101,"aaa"));
+		// System.out.println(h);
+		// h.add(101);
+		// System.out.println(h);
+		
+			// Case3
+		// h.add(new Employee(101,"aaa"));
+		// System.out.println(h);
+		// h.add(101);
+		// System.out.println(h);
+		// h.add(new Employee(101,"aaa"));
+		// System.out.println(h);
+		
+		// Case 4
+		// h.add(new Employee(101,"aaa"));
+		// System.out.println(h);
+		// h.add(101);
+		// System.out.println(h);
+		// h.add(new Employee(101,"bbb"));
+		// System.out.println(h);
+		
+		// Case 5
+		// h.add(new Employee(101,"aaa"));
+		// h.add(101);
+		// h.add(new Employee(101,"aaa"));
+		// h.add(new Employee(101,"bbb"));
+		
+		// Case 6
+		// h.add(new Employee(101,"aaa"));
+		// h.add(new Employee(101,"aaa"));
+		// h.add(new Employee(101,"bbb"));
+		// h.add(101);
+		// h.add(new Employee(101,"ddd"));
+		// System.out.println(h);
+	}
+}
+class Employee
+{
+	int id;
+	String name;
+	Employee(int id,String name)
+	{
+		this.id=id;
+		this.name=name;
+	}
+	public String toString()
+	{
+			return "id="+id+"\t name="+name;
+	}
+	
+	public int hashCode()
+	{
+		System.out.println("HashCode");
+		return id;
+	}
+	
+	public boolean equals(Object o)
+	{
+		System.out.println("Equals");
+		Employee e=(Employee)o;
+		if(id==e.id && name.equals(e.name))
+			return true;
+		else
+			return false;
 	}
 }
